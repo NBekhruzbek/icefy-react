83ba0c711048f0eda23357fd695223d05e023149
NBekhruzbek <bekhruzbek2022@gmail.com>
2026-01-25 05:06:57 +0900

    feat: develop HomeNavbar part one

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,13 +3,15 @@ import { Route, Switch, useLocation } from "react-router-dom";
 import { HomePage } from "./screens/homePage";
 import { AboutUsPage } from "./screens/aboutUsPage";
 import { ProductsPage } from "./screens/productsPage";
+import { OrdersPage } from "./screens/ordersPage";
 import { BlogPage } from "./screens/blogPage";
 import { HelpPage } from "./screens/helpPage";
 import { HomeNavbar } from "./components/headers/HomeNavbar";
 import { OtherNavbar } from "./components/headers/OtherNavbar";
-import { Footer } from "./components/footer";
+import Footer from "./components/footer";
 import { UsersPage } from "./screens/userPage";
 import "../css/app.css";
+import "../css/navbar.css";
 
 function App() {
   const location = useLocation();
@@ -23,6 +25,9 @@ function App() {
         </Route>
         <Route path="/products">
           <ProductsPage />
+        </Route>
+        <Route path="/orders">
+          <OrdersPage />
         </Route>
         <Route path="/blog-page">
           <BlogPage />
--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -1,3 +1,97 @@
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
 export function HomeNavbar() {
-  return <div>HomeNavbar</div>;
+  const authMember = null;
+  return (
+    <div className="home-navbar">
+      <div className="video-bg">
+        <video autoPlay muted loop playsInline>
+          <source src="/video/header2.mp4" type="video/mp4" />
+          Your browser does not support HTML5 video.
+        </video>
+      </div>
+      <Container sx={{ mt: "55px", height: "643px" }}>
+        <Stack
+          sx={{ height: "50px" }}
+          flexDirection={"row"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+        >
+          <Box>
+            <Typography>
+              <NavLink to="/" className={"icefy-logo"}>
+                <img
+                  style={{ width: "80px", height: "80px " }}
+                  src="/icons/logo.png"
+                />
+                Icefy
+              </NavLink>
+            </Typography>
+          </Box>
+          <Stack
+            flexDirection={"row"}
+            justifyContent={"space-between"}
+            minWidth={"700px"}
+            alignItems={"center"}
+          >
+            <Box className={"nav-txt hover-line"}>
+              <NavLink to="/" activeClassName={"underline"}>
+                Home
+              </NavLink>
+            </Box>
+            <Box className={"nav-txt hover-line"}>
+              <NavLink to="/about" activeClassName={"underline"}>
+                About Us
+              </NavLink>
+            </Box>
+            <Box className={"nav-txt hover-line"}>
+              <NavLink to="/prducts" activeClassName={"underline"}>
+                Products
+              </NavLink>
+            </Box>
+            {!authMember ? (
+              <Box className={"nav-txt hover-line"}>
+                <NavLink to="/orders" activeClassName={"underline"}>
+                  Orders
+                </NavLink>
+              </Box>
+            ) : null}
+            <Box className={"nav-txt hover-line"}>
+              <NavLink to="/blog-page" activeClassName={"underline"}>
+                Blogs
+              </NavLink>
+            </Box>
+            {!authMember ? (
+              <Box className={"nav-txt hover-line"}>
+                <NavLink to="/member-page" activeClassName={"underline"}>
+                  My Page
+                </NavLink>
+              </Box>
+            ) : null}
+            <Box className={"nav-txt hover-line"}>
+              <NavLink to="/help" activeClassName={"underline"}>
+                Help
+              </NavLink>
+            </Box>
+            {/** BASKET */}
+
+            {!authMember ? (
+              <Box>
+                <Button
+                  variant="contained"
+                  style={{ background: "#F83D8E", color: "#f8f8ff" }}
+                >
+                  Login
+                </Button>
+              </Box>
+            ) : (
+              <img />
+            )}
+          </Stack>
+        </Stack>
+        <Stack>DETAIL</Stack>
+      </Container>
+    </div>
+  );
 }
