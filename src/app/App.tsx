737447ffcdd93a9e0ad9db3f720b6e03f8466181
NBekhruzbek <bekhruzbek2022@gmail.com>
2026-01-24 23:16:05 +0900

    feat: MUI standard integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,24 @@
 import React from "react";
+import Container from "@mui/material/Container";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import "../css/app.css";
 
 function App() {
-  return <div>Icefy homePage</div>;
+  return (
+    <Container maxWidth="sm">
+      <Stack flexDirection={"column"}>
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component={"h4"}>
+            Create React App on TypeScript with REDUX
+          </Typography>
+        </Box>
+        <Button variant="contained">Contained</Button>
+      </Stack>
+    </Container>
+  );
 }
 
 export default App;
