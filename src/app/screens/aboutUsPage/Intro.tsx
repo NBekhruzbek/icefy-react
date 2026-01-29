77a7f80b7a0a7bff6fd5fa5bab64ac65d6ff4ea5
NBekhruzbek <bekhruzbek2022@gmail.com>
2026-01-29 21:11:32 +0900

    fix: modify aboutUsIntro sectional component

--- src/app/screens/aboutUsPage/Intro.tsx
+++ src/app/screens/aboutUsPage/Intro.tsx
@@ -1,9 +1,49 @@
-import { Container } from "@mui/material";
+import { Box, Button, Container, Stack } from "@mui/material";
+import { NavLink } from "react-router-dom";
 
 export default function Intro() {
   return (
     <div>
-      <Container>Intro Page</Container>
+      <Container>
+        <Stack className="about-intro">
+          <Box>
+            <img
+              src="/img/aboutUsIntro.jpg"
+              alt="Your browser does not support the img!"
+              className={"intro-img"}
+            />
+          </Box>
+          <Stack className="txt-area">
+            <Box className={"main-txt"}>
+              <Box component={"span"}>Our </Box>
+              <Box component={"span"} color={"#F83D8E"}>
+                Journey{" "}
+              </Box>
+              <Box component={"span"}>Began With a Simple Dream</Box>
+            </Box>
+            <Box className={"extra-txt"}>
+              Our goal is to make the best ice cream using only the finest,
+              natural ingredients. From rich, creamy classics to adventurous new
+              creations, every flavor is meticulously crafted in-house to ensure
+              the highest quality and freshness.
+            </Box>
+            <Box className={"extra-txt"}>
+              We take pride in offering a diverse range of options, including
+              dairy-free, vegan, and gluten-free choices, so everyone can find
+              their perfect scoop.
+            </Box>
+
+            <NavLink
+              to={"/blog-page"}
+              style={{ width: "192.05px", textDecoration: "none" }}
+            >
+              <Button variant="contained" className="button">
+                Read More
+              </Button>
+            </NavLink>
+          </Stack>
+        </Stack>
+      </Container>
     </div>
   );
 }
