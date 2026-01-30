4b78b55b561344c8b34addb01ea57fd2f2fce1d7
NBekhruzbek <bekhruzbek2022@gmail.com>
2026-01-30 10:08:49 +0900

    feat: organize products related sectional components

--- src/app/screens/productsPage/index.tsx
+++ src/app/screens/productsPage/index.tsx
@@ -1,5 +1,21 @@
 import { Container } from "@mui/material";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
+import ChosenProduct from "./ChosenProduct";
+import Products from "./Products";
+import "../../../css/products.css";
 
 export function ProductsPage() {
-  return <Container>ProductsPage</Container>;
+  const products = useRouteMatch();
+  return (
+    <div>
+      <Switch>
+        <Route path={`${products.path}/:productId`}>
+          <ChosenProduct />
+        </Route>
+        <Route path={`${products.path}`}>
+          <Products />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
