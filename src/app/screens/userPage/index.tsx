46ac925cdf47d647ecf463b8bdd1744d073a040e
NBekhruzbek <bekhruzbek2022@gmail.com>
2026-02-18 22:13:32 +0900

    fix: modify userPage component business logic for adding liked Products

--- src/app/screens/userPage/index.tsx
+++ src/app/screens/userPage/index.tsx
@@ -1,4 +1,5 @@
-import { Box, Container, Stack } from "@mui/material";
+import React from "react";
+import { Box, Container, Stack, Tab, Tabs } from "@mui/material";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TelegramIcon from "@mui/icons-material/Telegram";
@@ -9,10 +10,18 @@ import "../../../css/userPage.css";
 import { useGlobals } from "../../hooks/useGlobals";
 import { serverApi } from "../../../lib/config";
 import { MemberType } from "../../../lib/enums/member.enum";
+import { TabContext, TabPanel } from "@mui/lab";
 
 export function UsersPage() {
   const navigate = useNavigate();
   const { authMember } = useGlobals();
+
+  const [value, setValue] = React.useState("1");
+
+  /** HANDLERS **/
+  const handleChange = (e: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
 
   if (!authMember) navigate("/");
 
@@ -20,13 +29,63 @@ export function UsersPage() {
     <div className={"user-page"}>
       <Container>
         <Stack className={"my-page-frame"}>
-          <Stack className={"my-page-left"}>
-            <Box display={"flex"} flexDirection={"column"}>
-              <Box className={"menu-name"}>Modify Member Details</Box>
-              <Box className={"menu-content"}>
-                <Settings />
+          <Stack
+            className={"my-page-left"}
+            sx={{
+              borderRadius: "20px",
+              background:
+                "linear-gradient(106deg, #efd7ef 8%, #f5f9fc 40%, #f8eae1 66%, #eaf8f9 91%)",
+            }}
+          >
+            <TabContext value={value}>
+              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                <Tabs
+                  value={value}
+                  onChange={handleChange}
+                  aria-label="lab API tabs example"
+                  className={"table_list"}
+                  sx={{
+                    borderTopLeftRadius: "15px",
+                    borderTopRightRadius: "15px",
+                    background:
+                      "linear-gradient(90deg, #667eea 0%, #764ba2 100%)",
+                  }}
+                >
+                  <Tab
+                    label="Modify Member Details"
+                    value={"1"}
+                    sx={{
+                      borderTopLeftRadius: "15px",
+                      borderTopRightRadius: "15px",
+                      color: "white !important",
+                      fontWeight: "600",
+                      backgroundColor:
+                        value === "1" ? "#F768A6" : "transparent",
+                    }}
+                  />
+                  <Tab
+                    label="Liked Products"
+                    value={"2"}
+                    sx={{
+                      borderTopLeftRadius: "15px",
+                      borderTopRightRadius: "15px",
+                      color: "white !important",
+                      fontWeight: "600",
+                      backgroundColor:
+                        value === "2" ? "#F768A6" : "transparent",
+                    }}
+                  />
+                </Tabs>
               </Box>
-            </Box>
+              <Stack>
+                <Stack>
+                  <TabPanel value={"1"}>
+                    <Settings />
+                  </TabPanel>
+                  <TabPanel value={"2"}>{/* <LikedProducts /> */}</TabPanel>
+                </Stack>
+              </Stack>
+            </TabContext>
           </Stack>
 
           <Stack className={"my-page-right"}>
